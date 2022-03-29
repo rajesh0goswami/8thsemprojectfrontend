66556281d360078cfddcf2fec0749d52f817a40c
rajesh0goswami <cse.180310017@silicon.ac.in>
2022-03-29 12:25:48 +0530

    payment progress commit

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -7,33 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentComponent implements OnInit {
 
-  // paymentRequest!:google.payments.api.PaymentDataRequest;
+  price:Number
+
+  paymentRequest!:google.payments.api.PaymentDataRequest;
 
 
  
   constructor() { }
 
   ngOnInit(): void {
-  //   this.paymentRequest={
-  //     apiVersion:2,
-  //     apiVersionMinor:0,
-  //     allowedPaymentMethods:[
-  //       {
-  //         type:'CARD',
-  //         parameters:{
-  //           allowedAuthMethods:['PAN_ONLY','CRYPTOGRAM_3DS'],
-  //           allowedCardNetworks:['MASTERCARD','VISA'],
-  //         },
-  //         tokenizationSpecification:{
-  //           type:'PAYMENT_GATEWAY',
-  //           parameters:{
-  //             gateway:'example',
-  //             gatewayMerchantId:'exampleGateWayMerchantId',
-  //           },
-  //         },
-  //       },
-  //     ],
-  //   }
+    this.paymentRequest={
+      apiVersion:2,
+      apiVersionMinor:0,
+      allowedPaymentMethods:[
+        {
+          type:'CARD',
+          parameters:{
+            allowedAuthMethods:['PAN_ONLY','CRYPTOGRAM_3DS'],
+            allowedCardNetworks:['MASTERCARD','VISA'],
+          },
+          tokenizationSpecification:{
+            type:'PAYMENT_GATEWAY',
+            parameters:{
+              gateway:'example',
+              gatewayMerchantId:'exampleGateWayMerchantId',
+            },
+          },
+        },
+      ],
+      merchantInfo:{
+        merchantId:'BCR2DN4T6DHPHHI2',
+        merchantName:'ABCcompany',
+      },
+      transactionInfo:{
+        totalPriceStatus:'FINAL',
+        totalPriceLabel:'Total',
+        totalPrice:this.price.toFixed(2),
+        currencyCode:'INR',
+        countryCode:'IND',
+
+      },
+    }
   }
+
+ async onLoadPaymentData(event:Event)  {
+   const paymentData=(event as CustomEvent<google.payments.api.PaymentData>).detail;
+
+   }
 
 }
